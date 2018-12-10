36672bd7412c4757988ac6d672df83c15230bfde
Flavio <ffroliva@gmail.com>
2018-12-10 00:07:11 -0200

    UI Implementation.

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -2,6 +2,8 @@
 'use strict'
 var app = angular.module('app', ['app.service']);
 app.constant("CONSTANTS", {
+    apiKey: APIKEY,
+    skyscannerApi: 'http://partners.api.skyscanner.net/',
     getCustomersWithinDistance: "/customers/within-distance/",
     getAllCustomers: "/customers"
 });
